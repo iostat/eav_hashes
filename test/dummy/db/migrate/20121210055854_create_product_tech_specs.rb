793d5739b3f8b291175fd8207dd82a95e712a760
Ilya Ostrovskiy <ilya@200proof.cc>
2012-12-10 01:06:06 -0500

    forgot to add the generated migration in the dummy app

---
+++ test/dummy/db/migrate/20121210055854_create_product_tech_specs.rb
@@ -0,0 +1,16 @@
+class CreateProductTechSpecs < ActiveRecord::Migration
+  def change
+    create_table :product_tech_specs do |t|
+      t.references :product, :null => false
+      t.string :entry_key, :null => false
+      t.text :value, :null => false
+      t.integer :value_type, :null => false
+      t.boolean :symbol_key, :null => false, :default => true
+
+      t.timestamps
+    end
+
+    add_index :product_tech_specs, :product_id
+    add_index :product_tech_specs, :entry_key
+  end
+end
